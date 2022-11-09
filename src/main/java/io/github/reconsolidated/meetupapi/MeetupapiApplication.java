602e90e5c64207c3e5b1d96a40dcb09ed797a7f1
reconsolidated <gracjanpasik@gmail.com>
2022-11-09 17:15:40 +0100

    Initial commit

---
+++ src/main/java/io/github/reconsolidated/meetupapi/MeetupapiApplication.java
@@ -0,0 +1,13 @@
+package io.github.reconsolidated.meetupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetupapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/reconsolidated/meetupapi/MeetupapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.reconsolidated.meetupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetupapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
