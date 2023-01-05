098e0da626168cf83f7b5e98c2dd965bfce870a7
ReConsolidated <gracjanpasik@gmail.com>
2023-01-05 21:11:05 +0100

    fix friends

--- src/main/java/io/github/reconsolidated/meetupapi/friends/FriendsService.java
+++ src/main/java/io/github/reconsolidated/meetupapi/friends/FriendsService.java
@@ -16,9 +16,9 @@ public class FriendsService {
     private final AppUserService appUserService;
 
     public void addFriend(Long friend1, Long friend2) {
-        Friends friends = friendsRepository.findById(friend1).orElse(new Friends());
+        Friends friends = friendsRepository.findById(friend1).orElse(new Friends(friend1));
         friends.getFriendList().add(appUserService.getById(friend1).orElseThrow());
-        Friends friends2 = friendsRepository.findById(friend2).orElse(new Friends());
+        Friends friends2 = friendsRepository.findById(friend2).orElse(new Friends(friend2));
         friends2.getFriendList().add(appUserService.getById(friend1).orElseThrow());
         friendsRepository.save(friends);
         friendsRepository.save(friends2);
