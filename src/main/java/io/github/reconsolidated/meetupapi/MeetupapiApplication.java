f885ce1e07b5f0acd7e74d01b033f07fd0cb1059
ReConsolidated <gracjanpasik@gmail.com>
2023-01-05 21:04:44 +0100

    fix currentuser resolver

--- src/main/java/io/github/reconsolidated/meetupapi/MeetupapiApplication.java
+++ src/main/java/io/github/reconsolidated/meetupapi/MeetupapiApplication.java
@@ -1,9 +1,15 @@
 package io.github.reconsolidated.meetupapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableWebMvc
 public class MeetupapiApplication {
 
 	public static void main(String[] args) {
